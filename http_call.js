c79cff3d01132acf0b6b8106b1ce7fe655d40717
inlbehera <lalit143.er@gmail.com>
2018-05-13 14:22:02 +0530

    http call added

--- http_call.js
+++ http_call.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const request = require('request');
 
 function parse() {
   https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
@@ -17,5 +18,14 @@ function parse() {
     console.log(`Error: ${err.message}`);
   });
 }
+function parseWithRequestApi() {
+  request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true },
+    (err, res, body) => {
+      if (err) { return console.log(err); }
+      console.log(body.url);
+      console.log(body.explanation);
+    });
+}
 
-parse();
+// parse();
+parseWithRequestApi();
