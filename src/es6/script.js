1debcae04651509f55a442e569e701ea74ed8a04
lkb <lalitkbehera@gmail.com>
2018-07-04 15:36:55 +0530

    class in js added

--- src/es6/script.js
+++ src/es6/script.js
@@ -1,10 +1,15 @@
+// new es6 class syntax
 class User {
   constructor(username, email, password) {
     this.username = username;
     this.email = email;
     this.password = password;
   }
+  // static mehtod
+  static count() {
 
+  }
+  // method
   register() {
     console.log(`${this.username} is now registerd`);
   }
@@ -12,6 +17,7 @@ class User {
 // const bob = new User('bob', 'bob@gmail.com', '1234');
 // bob.register();
 
+// inheritance in es6
 class Member extends User {
   constructor(username, email, password, memberpackage) {
     super(username, email, password);
@@ -26,3 +32,30 @@ class Member extends User {
 const mike = new Member('mike', 'mike@gmail', '1234', 'standard');
 mike.getpackage();
 mike.register();
+
+const theString = 'Hello, my name is Jhon and I loveJavaScript';
+
+// startsWith()
+// endsWith()
+// includes()
+
+// console.log(theString.startsWith('Hello'));
+// console.log(theString.endsWith('JavaScript'));
+console.log(theString.includes('love'));
+
+// default params
+// function greet($greeting = 'He there!') {
+//   console.log($greeting);
+// }
+
+// greet();
+
+
+const args = [1, 2, 3];
+const args2 = [6, 7, 8];
+function test() {
+  console.log(args);
+}
+// spread operator or in java vararg
+test(...args, ...args2);
+
