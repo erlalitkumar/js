912d011801b16accbc103e82c8df784786daa45b
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-10-25 13:29:33 +0530

    rxjs: simple observer demo code

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
 import * as Rx from "rxjs/Observable";
+import { Observable } from "rxjs/Observable";
 
-console.log(Rx);
+var observable = Observable.create((observer:any)=>{
+    observer.next("Hi There")
+    observer.next("HOw are you")
+    observer.complete()
+    observer.next("HOw are you 3")
+});
+
+observable.subscribe((x:any) => addItem(x), (error:any)=> addItem(error),()=>addItem("completed"));
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
