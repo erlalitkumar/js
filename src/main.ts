3129b7f5fe78fe16dc7e9d4475f9e0e5c21856b6
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-10-25 13:36:26 +0530

    rxjs: how to unsubscribe a subscription

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,33 @@
 import * as Rx from "rxjs/Observable";
 import { Observable } from "rxjs/Observable";
 
-var observable = Observable.create((observer:any)=>{
-    observer.next("Hi There")
-    observer.next("HOw are you")
-    observer.complete()
-    observer.next("HOw are you 3")
+var observable = Observable.create((observer: any) => {
+  try {
+    observer.next("Hi There");
+    observer.next("HOw are you");
+    setInterval(() => {
+      observer.next("I am good");
+    }, 2000);
+    // observer.complete();
+    // observer.next("HOw are you 3");
+  } catch (err) {
+    observer.error(err);
+  }
 });
 
-observable.subscribe((x:any) => addItem(x), (error:any)=> addItem(error),()=>addItem("completed"));
+var observer = observable.subscribe(
+  (x: any) => addItem(x),
+  (error: any) => addItem(error),
+  () => addItem("completed")
+);
 
-function addItem(val:any) {
-    var node = document.createElement("li");
-    var textnode = document.createTextNode(val);
-    node.appendChild(textnode);
-    document.getElementById("output").appendChild(node);
+setTimeout(()=>{
+    observer.unsubscribe();
+},6001)
+
+function addItem(val: any) {
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById("output").appendChild(node);
 }
